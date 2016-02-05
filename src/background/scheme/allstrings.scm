196143628a6cd8fa1aacc7b3f47b876ea369fcfe
Jim Hefferon <jhefferon@smcvt.edu>
2016-02-05 13:18:34 -0500

    add allstrings.scm to generate all programs

---
+++ src/background/scheme/allstrings.scm
@@ -0,0 +1,38 @@
+;; Generate all bit strings of length n
+
+;; Display all bitstrings of the given length
+(define (bitstrings n lst)
+  (if (= n 0)
+      (begin
+	(display (list->string lst))
+	(newline))
+      (begin
+	(bitstrings (- n 1) (cons #\0 lst))
+	(bitstrings (- n 1) (cons #\1 lst)))
+      ))
+
+;; Constant of all characters in the alphabet
+(define ALLCHARS '(#\a #\b #\c #\space #\( #\)))
+
+;; Display a list of all strings of characters from ALLCHARS
+(define (allstrings n lst)
+  (define (do-one ch)
+    (allstrings (- n 1) (cons ch lst)))
+
+  (if (= n 0)
+      (begin
+	(display (list->string lst))
+	(newline))
+      (begin
+	(map do-one ALLCHARS)
+	'())))
+
+(define OUTFN "allstringstest.scm")
+
+(require-extension shell)   ; run programs from command line 
+(define (run-one-string s)
+  (let ((p (open-output-file OUTFN)))
+    (display s p)
+    (close-output-port p))
+  (let ((rc (run* ("cat" "<" ,OUTFN ">" "test.out"))))
+    (display (string-append "return code is " (number->string rc)))))

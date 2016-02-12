3631c79bd34e381cb95f7c8c8166dd38bbb05d4b
Jim Hefferon <jhefferon@smcvt.edu>
2016-02-12 13:15:17 -0500

    move pairing function to subsection on numbering

--- src/background/scheme/allstrings.scm
+++ src/background/scheme/allstrings.scm
@@ -4,7 +4,7 @@
 (define (bitstrings n lst)
   (if (= n 0)
       (begin
-	(display (list->string lst))
+	(display (list->string (reverse lst)))
 	(newline))
       (begin
 	(bitstrings (- n 1) (cons #\0 lst))
@@ -21,18 +21,53 @@
 
   (if (= n 0)
       (begin
-	(display (list->string lst))
+	(display (list->string (reverse lst)))
 	(display #\,))
       (begin
 	(map do-one ALLCHARS)
-	'())))
+	'())))  ; return empty list
 
+(define (all-strings n lst)
+  (define (do-one ch)
+    (allstrings (- n 1) (cons ch lst)))
+
+  (if (= n 0)
+      (list->string (reverse lst))
+      (begin
+	(map do-one ALLCHARS)
+	'())))  ; return empty list
+
+;; Name of the output file
 (define OUTFN "allstringstest.scm")
 
-(require-extension shell)   ; run programs from command line 
+;; Allow us to run programs from the command line
+(require-extension shell) 
+
+;; Write the input string to a file, run compiler on that, and test 
+;; the return code from the compiler. 
+;; (define (run-one-string s)
+;;   (let ((p (open-output-file OUTFN)))
+;;     (display s p)
+;;     (close-output-port p))
+;;   (let ((rc (run* ("cat" "<" ,OUTFN ">" "test.out"))))
+;;     (display (string-append "return code is " (number->string rc)))))
+
+
 (define (run-one-string s)
   (let ((p (open-output-file OUTFN)))
     (display s p)
     (close-output-port p))
-  (let ((rc (run* ("cat" "<" ,OUTFN ">" "test.out"))))
-    (display (string-append "return code is " (number->string rc)))))
+  (let ((rc (run* ("csc" ,OUTFN))))
+    (if (= rc 0)
+	#t
+	#f)))
+
+(define (run-strings-helper cnt string-list)
+  (if (null? string-list)
+      cnt
+      (begin
+	(if (run-one-string (car string-list))
+	    (run-strings-helper (+ 1 cnt) (cdr string-list))
+	    (run-strings-helper cnt (cdr string-list))))))
+(define (run-strings string-list)
+  (display (run-strings-helper 0 string-list)))

237c70d5327ebe0d3c395ad96b260e320f6c24b3
Jim Hefferon <jhefferon@smcvt.edu>
2016-02-20 10:37:59 -0500

    drop code into subsection

--- src/background/scheme/godelnumbering.scm
+++ src/background/scheme/godelnumbering.scm
@@ -4,38 +4,66 @@
 	n)
      2))
 
-;; g  Godel number of the pair (x,y) of integers
-(define (g x y)
+;; cantor  Cantor number of the pair (x,y) of integers
+(define (cantor x y)
   (let ((d (+ x y)))
     (+ (triangle-num d)
        x)))
 
-;; diag-num  given Godel number, find the number of the diagonal
-(define (diag-num g)
+;; diag-num  given Cantor number, find the number of the diagonal
+;; todo: get numerical errors if c is too large, change this to (use numbers)
+(define (diag-num c)
   (inexact->exact (floor (/
-			  (- (sqrt (+ (* 8 g) 1))
+			  (- (sqrt (+ (* 8 c) 1))
 			     1)
 			  2))))
 
-;; xy  given the godel number, return (x y) 
-(define (xy g)
-  (let* ((d (diag-num g))
+;; xy  given the cantor number, return (x y) 
+(define (xy c)
+  (let* ((d (diag-num c))
 	 (t (triangle-num d)))
-    (list (- g t)
-	  (- d (- g t)))))
+    (list (- c t)
+	  (- d (- c t)))))
 
-;; g3 number triples
-(define (g3 x0 x1 x2)
-  (g x0 (g x1 x2)))
+;; cantor-3 number triples
+(define (cantor-3 x0 x1 x2)
+  (cantor x0 (cantor x1 x2)))
 
-;; g4 number quads
-(define (g4 x0 x1 x2 x3)
-  (g x0 (g3 x1 x2 x3)))
+(define (xy-3 c)
+  (cons (car (xy c))
+	(xy (cadr (xy c)))))
 
-;; g-omega number any tuples
-(define (g-omega . args)
-  (cond ((null? args) (display "ERROR: g-omega requires an input"))
+;; cantor-4 number quads
+(define (cantor-4 x0 x1 x2 x3)
+  (cantor x0 (cantor-3 x1 x2 x3)))
+
+;; cantor-n number any-sized tuple
+(define (cantor-n . args)
+  (cond ((null? args) (display "ERROR: cantor-omega requires an input"))
 	((= 1 (length args)) (car args))
-	((= 2 (length args)) (g (car args) (cadr args)))
+	((= 2 (length args)) (cantor (car args) (cadr args)))
 	(else 
-	 (g (car args) (apply g-omega (cdr args))))))
+	 (cantor (car args) (apply cantor-n (cdr args))))))
+
+;; cantor-omega encode the arity of the first component, so xy-omega can get it
+(define (cantor-omega . tuple)
+  (if (null? tuple)
+      (display "ERROR: cantor-omega requires a nonempty tuple")
+      (let ((newtuple (cons (length tuple) 
+			    (apply cantor-n tuple))))
+	(apply cantor newtuple))))
+
+;; xy-arity  return the tuple of the given arity making the cantor number c
+(define (xy-arity arity c)
+  (if (= 1 arity)
+      (list c)
+      (cons (car (xy c))
+	    (xy-arity (- arity 1) (cadr (xy c))))))
+
+;; xy-omega  interpret c as a pair (arity cantor-number) and return the tuple
+;;  of that arity with that cantor number
+(define (xy-omega c)
+  (let* ((pair (xy c))
+	 (arity (car pair))
+	 (cantor-number (cadr pair)))
+    (xy-arity arity cantor-number)))
--- src/background/scheme/godelnumbering_test.scm
+++ src/background/scheme/godelnumbering_test.scm
@@ -14,44 +14,126 @@
 
 ;; ======= diagonal enumeration
 (test-begin "diagonal enumeration")
-(test #t (= 0 (g 0 0)))
-(test #t (= 1 (g 0 1)))
-(test #t (= 2 (g 1 0)))
-(test #t (= 3 (g 0 2)))
-(test #t (= 4 (g 1 1)))
+(test #t (= 0 (cantor 0 0)))
+(test #t (= 1 (cantor 0 1)))
+(test #t (= 2 (cantor 1 0)))
+(test #t (= 3 (cantor 0 2)))
+(test #t (= 4 (cantor 1 1)))
 (do ((i 0 (+ i 1)))
     ((= i 10) '())
-  (test #t (= (triangle-num i) (g 0 i))))
+  (test #t (= (triangle-num i) (cantor 0 i))))
 (test-end "diagonal enumeration")
 
 
 ;; ======= diagonal number from diagonal enumeration
 (test-begin "diagonal number from enumeration")
-(test #t (= 0 (diag-num (g 0 0))))  ; (0,0) is on diagonal 0
-(test #t (= 1 (diag-num (g 0 1))))  ; (0,1) is on diagonal 1
-(test #t (= 1 (diag-num (g 1 0))))  ; 
-(test #t (= 2 (diag-num (g 0 2))))  ; 
-(test #t (= 2 (diag-num (g 1 1))))  ; 
+(test #t (= 0 (diag-num (cantor 0 0))))  ; (0,0) is on diagonal 0
+(test #t (= 1 (diag-num (cantor 0 1))))  ; (0,1) is on diagonal 1
+(test #t (= 1 (diag-num (cantor 1 0))))  ; 
+(test #t (= 2 (diag-num (cantor 0 2))))  ; 
+(test #t (= 2 (diag-num (cantor 1 1))))  ; 
 (do ((x 0 (+ x 1))
      (y 0 (+ y 1)))
     ((= (+ x y) 10) '())
-  (test #t (= (+ x y) (diag-num (g x y)))))
+  (test #t (= (+ x y) (diag-num (cantor x y)))))
 (test-end "diagonal number from enumeration")
 
 
 ;; ======= xy from diagonal enumeration
 (test-begin "xy from enumeration")
-(test #t (equal? '(0 0) (xy (g 0 0))))  
-(test #t (equal? '(0 1) (xy (g 0 1))))  
-(test #t (equal? '(1 0) (xy (g 1 0))))  
-(test #t (equal? '(0 2) (xy (g 0 2))))  
-(test #t (equal? '(1 1) (xy (g 1 1))))  
-(test #t (equal? '(2 0) (xy (g 2 0))))  
+(test #t (equal? '(0 0) (xy (cantor 0 0))))  
+(test #t (equal? '(0 1) (xy (cantor 0 1))))  
+(test #t (equal? '(1 0) (xy (cantor 1 0))))  
+(test #t (equal? '(0 2) (xy (cantor 0 2))))  
+(test #t (equal? '(1 1) (xy (cantor 1 1))))  
+(test #t (equal? '(2 0) (xy (cantor 2 0))))  
 (do ((x 0 (+ x 1))
      (y 0 (+ y 1)))
     ((= (+ x y) 10) '())
-  (test #t (equal? (list x y) (xy (g x y)))))
+  (test #t (equal? (list x y) (xy (cantor x y)))))
 (test-end "xy from enumeration")
+
+
+;; ======= cantor-3 and xy-3 
+(test-begin "cantor-3 and xy-3")
+(test #t (equal? '(0 0 0) (xy-3 (cantor-3 0 0 0))))  
+(test #t (equal? '(0 0 1) (xy-3 (cantor-3 0 0 1))))  
+(test #t (equal? '(0 1 0) (xy-3 (cantor-3 0 1 0))))  
+(test #t (equal? '(1 0 0) (xy-3 (cantor-3 1 0 0))))  
+(test #t (equal? '(1 2 3) (xy-3 (cantor-3 1 2 3))))  
+(do ((x 0 (+ x 1)))
+    ((>= x 5) x)
+  (do ((y 0 (+ y 1)))
+      ((>= y 5) y)
+    (do ((z 0 (+ z 1)))
+	((>= z 5) '())
+      (test (string-append "case: x=" (number->string x)
+			   " y=" (number->string y)
+			   " z=" (number->string z))
+	    #t (equal? (list x y z) (xy-3 (cantor-3 x y z)))))))
+(test-end "cantor-3 and xy-3")
+
+
+
+;; ======= xy-arity 2 returns the same as xy? 
+(test-begin "xy-arity and xy")
+(test #t (equal? (xy 0) (xy-arity 2 0)))
+(test #t (equal? (xy 1) (xy-arity 2 1)))
+(test #t (equal? (xy 2) (xy-arity 2 2)))
+(test #t (equal? (xy 3) (xy-arity 2 3)))
+(do ((c 0 (+ c 1)))
+    ((>= c 100) c)
+  (test (string-append "case: c=" (number->string c))
+	#t (equal? (xy c) (xy-arity 2 c))))
+(test-end "xy-arity and xy")
+
+
+
+
+;; ======= cantor-3 and xy-arity 
+(test-begin "cantor-3 and xy-arity")
+(test #t (equal? '(0 0 0) (xy-arity 3 (cantor-3 0 0 0))))  
+(test #t (equal? '(0 0 1) (xy-arity 3 (cantor-3 0 0 1))))  
+(test #t (equal? '(0 1 0) (xy-arity 3 (cantor-3 0 1 0))))  
+(test #t (equal? '(1 0 0) (xy-arity 3 (cantor-3 1 0 0))))  
+(test #t (equal? '(1 2 3) (xy-arity 3 (cantor-3 1 2 3))))  
+(do ((x 0 (+ x 1)))
+    ((>= x 5) x)
+  (do ((y 0 (+ y 1)))
+      ((>= y 5) y)
+    (do ((z 0 (+ z 1)))
+	((>= z 5) '())
+      (test (string-append "case: x=" (number->string x)
+			   " y=" (number->string y)
+			   " z=" (number->string z))
+	    #t (equal? (list x y z) (xy-arity 3 (cantor-3 x y z)))))))
+(test-end "cantor-3 and xy-arity")
+
+
+;; helper: makelist-seed  make a list of length len containing all seed's
+(define (makelist-seed len seed)
+  (if (= len 1)
+     (list seed)                       
+     (cons seed (makelist-seed (- len 1) seed)))) 
+
+;; ======= cantor-n and xy-arity 
+(test-begin "cantor-n and xy-arity")
+(test #t (equal? '(0 0 0) (xy-arity 3 (cantor-n 0 0 0))))  
+(test #t (equal? '(0 0 1) (xy-arity 3 (cantor-n 0 0 1))))  
+(test #t (equal? '(0 1 0) (xy-arity 3 (cantor-n 0 1 0))))  
+(test #t (equal? '(1 0 0) (xy-arity 3 (cantor-n 1 0 0))))  
+(test #t (equal? '(1 2 3) (xy-arity 3 (cantor-n 1 2 3))))  
+(test #t (equal? '(0 0 0 0) (xy-arity 4 (cantor-n 0 0 0 0))))  
+(test #t (equal? '(0 0 0 1) (xy-arity 4 (cantor-n 0 0 0 1))))  
+(test #t (equal? '(1 2 3 4) (xy-arity 4 (cantor-n 1 2 3 4))))  
+(test #t (equal? '(1 0 0 0 5) (xy-arity 5 (cantor-n 1 0 0 0 5))))  
+(test #t (equal? '(1 2) (xy-arity 2 (cantor-n 1 2))))  
+(do ((n 2 (+ n 1)))
+    ((> n 10) n)
+  (let ((lst (makelist-seed n n)))
+    (test (string-append "case: n=" (number->string n))
+	  #t (equal? lst (xy-arity n (apply cantor-n lst))))))
+(test-end "cantor-n and xy-arity")
 
 
 

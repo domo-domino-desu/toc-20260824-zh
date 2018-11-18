4ba5fda33031c50ec1b87b5ba66d2c5951984c12
Jim Hefferon <jhefferon@smcvt.edu>
2018-11-18 16:44:16 -0500

    more on regexes in the wild, and some adjustments in wording elsewhere

--- src/automata/scheme/regexes.rkt
+++ src/automata/scheme/regexes.rkt
@@ -59,7 +59,6 @@
   )
 
 
-
 ;; Lists with \d \D, etc
 (define slash-d #px"^\\d$")
 (module+ test
@@ -68,4 +67,55 @@
   (check-true (regexp-match? slash-d "9"))
   (check-false (regexp-match? slash-d "a"))
   (check-false (regexp-match? slash-d "02"))
+  )
+
+
+;; Canadian postal codes
+(define canadian #px"^[a-zA-Z]\\d[a-zA-Z] \\d[a-zA-Z]\\d$")
+(module+ test
+  (check-true (regexp-match? canadian "A1B 2C3"))
+  (check-true (regexp-match? canadian "a1b 2c3"))
+  (check-true (regexp-match? canadian "f9f 9f9"))
+  (check-false (regexp-match? canadian "AAA AAA"))
+  (check-false (regexp-match? canadian "12345"))
+  )
+
+
+;; US ZIP postal codes
+(define zip #px"^\\d\\d\\d\\d\\d$")
+(module+ test
+  (check-true (regexp-match? zip "12345"))
+  (check-true (regexp-match? zip "11111"))
+  (check-false (regexp-match? zip "1234"))
+  (check-false (regexp-match? zip "1234567"))
+  (check-false (regexp-match? zip "abcde"))
+  )
+
+
+;; Twelve hour time format
+(define twelve #px"^(|0|1)\\d:\\d\\d\\s(am|pm)$")
+(module+ test
+  (check-true (regexp-match? twelve "8:05 am"))
+  (check-true (regexp-match? twelve "10:15 pm"))
+  (check-true (regexp-match? twelve "13:00 am"))
+  (check-true (regexp-match? twelve "9:61 pm"))
+  (check-false (regexp-match? twelve "8:05"))
+  (check-false (regexp-match? twelve "noon"))
+  (check-false (regexp-match? twelve "8 am"))
+  (check-false (regexp-match? twelve "23:31"))
+  )
+
+
+
+;; Fixed/long twelve hour time format
+(define twelve-long #px"^(01|02|03|04|05|06|07|08|09|1|2|3|4|5|6|7|8|9|10|11|12):(01|02|03|04|05|06|07|08|09|10|11|12|13|14|15|16|17|18|19|20|21|22|23|24|25|26|27|28|29|30|31|32|33|34|35|36\37|38|39|40|41|42|43|44|45|46|47|48|49|50|51|52|53|54|55|56|57|58|59|60)\\s(am|pm)$")
+(module+ test
+  (check-true (regexp-match? twelve-long "8:05 am"))
+  (check-true (regexp-match? twelve-long "10:15 pm"))
+  (check-false (regexp-match? twelve-long "13:00 am"))
+  (check-false (regexp-match? twelve-long "9:61 pm"))
+  (check-false (regexp-match? twelve-long "8:05"))
+  (check-false (regexp-match? twelve-long "noon"))
+  (check-false (regexp-match? twelve-long "8 am"))
+  (check-false (regexp-match? twelve-long "23:31"))
   )

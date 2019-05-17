814e71f1830d1db84998ff22d660342ea94382ae
Jim Hefferon <jhefferon@smcvt.edu>
2019-05-17 10:03:51 -0400

    start util to convert output from tm simulator to asy code

---
+++ bin/turing_machine/tm_to_asy.py
@@ -0,0 +1,160 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Convert output from turing machine simulator for use in Asymptote.
+"""
+__version__ = "0.0.1"
+__author__ = "Jim Hefferon"
+__license__ = "GPL 3.0"
+
+import sys
+import os, os.path
+import re, string
+import traceback, pprint
+import argparse
+import time
+
+# Global variables spare me from putting them in the call of each fcn.
+VERBOSE = False
+DEBUG = False
+
+PGM_ROOTNAME = os.path.splitext(os.path.basename(sys.argv[0]))[0]
+PGM_SRC_DIR = os.path.dirname(__file__)
+
+class JHException(Exception):
+    pass
+
+import logging
+# Potential logging levels: DEBUG | INFO | WARNING | ERROR | CRITICAL
+LOG_LEVEL_CHOICES=["debug", "info", "warning", "error", "critical", "default"]
+DEFAULT_LOG_LEVEL = "warning"
+if not(DEFAULT_LOG_LEVEL in LOG_LEVEL_CHOICES):
+    critical("DEFAULT_LOG_LEVEL "+str(DEFAULT_LOG_LEVEL)+ \
+             " must be in LOG_LEVEL_CHOICES="+str(LOG_LEVEL_CHOICES))
+
+def _set_log_level(log, choice=DEFAULT_LOG_LEVEL):
+    c = choice.casefold()  # like lower() but for case-insensitive matching
+    # log.debug('Logging level set to '+choice)
+    if (c == "debug"):
+        log.setLevel(logging.DEBUG)
+    elif (c == "info"):
+        log.setLevel(logging.INFO)
+    elif (c == "warning"):
+        log.setLevel(logging.WARNING)
+    elif (c == "error"):
+        log.setLevel(logging.ERROR)
+    elif (c == "critical"):
+        log.setLevel(logging.CRITICAL)
+    else:
+        error("Logging level {0!s} not known".format(choice))
+
+# Establish logging
+log = logging.getLogger(__name__)
+_set_log_level(log)
+# Log errors to the console
+log_sh = logging.StreamHandler(stream=sys.stderr)
+log_sh.setFormatter(logging.Formatter('%(levelname)s - Line: %(lineno)d\n  %(message)s'))
+_set_log_level(log_sh,"ERROR")
+log.addHandler(log_sh)
+# Log most everything to a file
+log_fh = logging.FileHandler(os.path.abspath(os.path.join(
+    os.path.dirname(__file__), 
+    os.path.basename(__file__).rstrip('.py') + '.log')),
+                         mode='w')
+log_fh.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - Line: %(lineno)d\n%(message)s'))
+_set_log_level(log_fh,"INFO")
+log.addHandler(log_fh)
+if DEBUG:
+    _set_log_level(log_sh,"DEBUG")
+    _set_log_level(log_fh,"DEBUG")
+
+def warning(s):
+    t = 'WARNING: '+s+"\n"
+    log.warning(t)
+
+def error(s, level=10):
+    t = 'ERROR: '+s+"\n"
+    log.error(t,exc_info=True)
+    sys.exit(level)
+
+def critical(s, level=1):
+    t = 'CRITICAL ERROR: '+s+"\n"
+    log.critical(t,exc_info=True)
+    sys.exit(level)
+
+# Format example: "q0:   BB*1*1101B "
+output_line_regex = r"\s*(q\d*):\s([^\*]*)\*([^\*]*)\*([^\*]*)"
+output_line_re = re.compile(output_line_regex, re.I)  # re.I in case capital q
+def parse_line(lne,line_no):
+    """Split one line into the consituitent parts
+      lne  string  line of output from Turing machine simulator
+    """
+    m = output_line_re.match(lne)
+    if m:
+        return {'state': m.group(1),
+                'prefix': m.group(2),
+                'currentchar': m.group(3),
+                'suffix': m.group(4)}
+    else:
+        # error("line number {0:d} not a regex match with {1:s}: {2:s}".format(line_no,output_line_re,lne))
+        error("line number {0:d} not a match: >>{1:s}<<".format(line_no,lne))
+        
+
+# ===========================================================
+def main(args):
+    test_line = "q0:   BB*1*1101B  "
+    d = parse_line(test_line,1)
+    print("state: {state}, prefix: {prefix}, current character: {currentchar}, suffix: {suffix}".format(**d))
+
+# ===========================================================
+if __name__ == '__main__':
+    try:
+        start_time = time.time()
+        # Parser: See http://docs.python.org/dev/library/argparse.html
+        parser = argparse.ArgumentParser(description=__doc__+
+                                         "  Author: "+__author__
+                                         +", Version: "+__version__
+                                         +", License: "+__license__)
+        parser.add_argument('-f', '--filename',
+                            action='store',
+                            default='tm_output.txt',
+                            help="File with output. Default: {0!s}".format(DEBUG))
+        parser.add_argument('-D', '--debug',
+                            action='store_true',
+                            default=DEBUG,
+                            help="Run debugging code. Default: {0!s}".format(DEBUG))
+        parser.add_argument('-L', '--log_level',
+                            action='store',
+                            type=str,
+                            choices=LOG_LEVEL_CHOICES,
+                            default=DEFAULT_LOG_LEVEL,
+                            help="Set the logging level. Default: {0!s}".format(DEFAULT_LOG_LEVEL))
+        parser.add_argument('-v', '--version',
+                            action='version',
+                            version=__version__)
+        parser.add_argument('-V', '--verbose',
+                            action='store_true',
+                            default=False,
+                            help='Give verbose output. Default: {0!s}'.format(VERBOSE))
+        log.info("{0!s} Started".format(parser.prog))
+        args = parser.parse_args()
+        _set_log_level(log,args.log_level)        
+        if args.debug:
+            _set_log_level(log_fh,"DEBUG")
+            _set_log_level(log,"DEBUG")
+        elif args.verbose:
+            _set_log_level(log_fh,"INFO")
+            _set_log_level(log,"INFO")
+        main(args)
+        _set_log_level(log,"INFO")
+        log.info("{0!s} Ended.  Elapsed time {1:0.2f} sec".format(parser.prog,time.time() - start_time))
+        sys.exit(0)
+    except KeyboardInterrupt as e:  # Ctrl-C
+        raise e
+    except SystemExit as e:  # sys.exit()
+        raise e
+    except Exception as e:
+        print('ERROR, UNEXPECTED EXCEPTION')
+        print(str(e))
+        traceback.print_exc()
+        os._exit(1)

cf506088c2f12b7923244484472526710e9e79f3
Jim Hefferon <jhefferon@smcvt.edu>
2021-05-11 14:45:53 -0400

    add more mu recursion exercises

--- src/scheme/prologue/mu-addition.rkt
+++ src/scheme/prologue/mu-addition.rkt
@@ -2,11 +2,13 @@
 ;; mu-addition Exercise on unbounded minimization
 
 (define (g x y)
-  (+ x y))
+  (if (= 100 (+ x y))
+      0
+      1))
 
 (define (f x)
   (define (f-helper x y)
-    (if (= 100 (g x y))
+    (if (= 0 (g x y))
         y
         (f-helper x (+ 1 y))))
   (let ([y 0])

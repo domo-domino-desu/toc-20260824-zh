a24a5d6f3b8045b6302d905be062eaf6524124aa
Jim Hefferon <jhefferon@smcvt.edu>
2022-10-19 13:16:36 -0400

    upgrade discussion of mu recursion

--- src/scheme/prologue/prime-generating-poly.rkt
+++ src/scheme/prologue/prime-generating-poly.rkt
@@ -1,23 +1,23 @@
 #lang racket
 ;; prime-generating-poly.rkt
 
-(require math/number-theory)  ;; provides prime? 
+(require math/number-theory)  ;; provides the routine prime? 
 
-;; p  Compute the value of the function given by y |->  x0 * y^2 + x1 * y + x2
+;; p  Compute the value of the function y |->  x0 * y^2 + x1 * y + x2
 (define (p x0 x1 x2 y)
   (+ (* x0 y y) (* x1 y) x2))
 
 (provide p)
 
-;; g  Test p's output for primality
+;; g  Test quadratic polynomial's output for primality
 (define (g x0 x1 x2 y)
-  (if (prime? (p x0 x1 x2 y))
+  (if (prime? (+ (* x0 y y) (* x1 y) x2))
       1
       0))
 
 (provide g)
 
-;; f  Unbounded search to test if quadratic poly with params generates only primes
+;; f  Unbounded search to test if quadratic poly generates only primes
 (define (f x0 x1 x2)
   (define (f-helper y)
     (if (= 0 (g x0 x1 x2 y))
@@ -27,3 +27,31 @@
     (f-helper 0))
 
 (provide f)
+
+;; Check if a number n is the sum of two primes
+;; returns i <= n such that i and n-i are prime; returns #f if no such i
+(define (gb-check n)
+  (for/first ([i (in-range 2 (add1 n))]
+              #:when (and (prime? i)
+                          (prime? (- n i))))
+    i)))
+
+;; demonstrate that for/first returns #f if when is never triggered
+(define (test-never)
+  (for/first ([i (in-range 2 10)]
+              #:when (< i 0))
+    i))
+
+;; Use gb-check to return predicate
+(define (gb-g n)
+  (if (gb-check (* 2 n))
+      1
+      0))
+
+(define (gb-f)
+  (define (gb-f-helper y)
+    (if (= 0 (gb-g y))
+        y
+        (gb-f-helper (add1 y))))
+  
+    (gb-f-helper 2)) ;; start with 4

da9aebad3705dde7d62714b0dcc566b5c3bd2c7a
Jim Hefferon <jhefferon@smcvt.edu>
2022-12-17 19:42:07 -0500

    sharpen defn of fsa computation

--- src/00build.sh
+++ src/00build.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 # LaTeX the Computation book and answers
 
+# tex4ht messes with this stuff
+rm prologue/prologue.aux
+rm background/background.aux
+rm languages/languages.aux
+rm automata/automata.aux
+rm complexity/complexity.aux
+rm appendix/appendix.aux
+
+
 # Generate the book; run it twice to settle future references
 pdflatex book
 makeindex -s book.ist -p odd book.idx

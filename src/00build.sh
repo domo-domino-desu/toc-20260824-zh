923e4eb5b69123cf950fd84417643bc53675cbb5
Jim Hefferon <jhefferon@smcvt.edu>
2022-12-21 13:35:51 -0500

    work through same issues for defintion of FSM

--- src/00build.sh
+++ src/00build.sh
@@ -2,12 +2,12 @@
 # LaTeX the Computation book and answers
 
 # tex4ht messes with this stuff
-rm prologue/prologue.aux
-rm background/background.aux
-rm languages/languages.aux
-rm automata/automata.aux
-rm complexity/complexity.aux
-rm appendix/appendix.aux
+# rm prologue/prologue.aux
+# rm background/background.aux
+# rm languages/languages.aux
+# rm automata/automata.aux
+# rm complexity/complexity.aux
+# rm appendix/appendix.aux
 
 
 # Generate the book; run it twice to settle future references

c8b7fa780aa67226492305eaa6f9f68d530c7559
Jim Hefferon <jhefferon@smcvt.edu>
2023-01-04 12:40:15 -0500

    developing pushdown machine simulator

--- src/scheme/automata/pushdown-machine.rkt
+++ src/scheme/automata/pushdown-machine.rkt
@@ -36,52 +36,107 @@
 (define ERROR -1) ;; 
 (provide HALT ERROR)
 
-;; ================= Configuration making and reading ==============
-;; A tape ends with an input end marker
+
+;; ======== Tape and stack =================
+;; A tape and stack are lists
+
+; list of characters  ->  tape-list
+; Make a tape list, ending with an input end marker
 (define (make-tape . tape-characters)
   (append tape-characters (list INPUTEND)))
 
-(provide make-tape)
+; list of characters  ->  character or ERROR
+; Get the first character on the tape.  Return ERROR if no such character.
+(define (tape-char tape-list)
+  (if (null? tape-list)
+      ERROR
+      (first tape-list)))
 
-;; A configuration is a list of three things:
-;;  the current state, as a natural number
-;;  the contents of the tape under and to the right of the head, as a list of tape characters
-;;  the contents of the stack, from the top down, including the BOT character.
-(define (make-config state tape-list stack-list)
-  (list state tape-list stack-list))
+; list of characters  ->  list of characters
+; Return the tape with the read head moved.
+(define (tape-shift tape-list)
+  (cdr tape-list))
 
-(define (get-current-state config) (first config))
-(define (get-tape-list config) (second config))
-(define (get-stack-list config) (third config))
-(define (get-current-symbol config)
-  (let([tape-list (get-tape-list config)])
-    (if (null? tape-list)
-        ERROR
-        (first tape-list))))
-(define (get-stack-top config)
-  (let([stack-list (get-stack-list config)])
-    (if (null? stack-list)
-        ERROR
-        (first stack-list))))
+(provide make-tape
+         tape-char
+         tape-shift)
 
-(provide make-config
-         get-current-state
-         get-tape-list
-         get-current-symbol
-         get-stack-list
-         get-stack-top)
+; list of characters -> stack-list
+; Make a stack, ending with a bottom marker
+(define (make-stack . stack-characters)
+  (append stack-characters (list BOT)))
 
+; stack-list ->  stack-list or ERROR
+; Pop the top character off the stack.  Return ERROR if stack is empty.
 (define (stack-pop stack-list)
   (if (null? stack-list)
       ERROR
       (cdr stack-list)))
+
+; stack-list ->  character or ERROR
+; Name top character on the stack, without popping.  Return ERROR if stack is empty.
+(define (stack-top stack-list)
+  (if (null? stack-list)
+      ERROR
+      (first stack-list)))
+
+; character stack-list ->  stack-list
+; Push a new top character onto the stack.
 (define (stack-push ch stack-list)
   (cons ch stack-list))
 
-(provide stack-pop
-         stack-push)
+; stack-list -> bool
+; Decide if the stack's top character is the bottom character.
+(define (stack-bot? stack-list)
+  (equal? BOT (car stack-list)))
 
-;; configuration-> string  Return a string showing the tape
+(provide make-stack
+         stack-pop
+         stack-top
+         stack-push
+         stack-bot?)
+
+
+;; ================= Configuration making and reading ==============
+;; A configuration is a list of three things:
+;;  the current state, as a natural number
+;;  the contents of the tape under and to the right of the head, as a list of tape characters
+;;  the contents of the stack, from the top down, including the BOT character.
+
+; natural-number character-list character-list -> list
+; Make a configuration (no test performed, say that tape characters are as declared)
+(define (make-config state tape-list stack-list)
+  (list state tape-list stack-list))
+
+; configuration  ->  natural number
+; Get the state number
+(define (get-current-state config) (first config))
+
+; configuration  -> list of characters
+; Get the tape
+(define (get-tape-list config) (second config))
+
+; configuration  ->  list of characeters
+; Get the stack
+(define (get-stack-list config) (third config))
+
+; configuration -> character or ERROR
+; Get the character pointed to by the read head
+(define (get-current-symbol config)
+  (tape-char (get-tape-list config)))
+
+; configuration -> character or ERROR
+(define (get-stack-top config)
+  (stack-top (get-stack-list config)))
+
+(provide make-config
+         get-current-state
+         get-tape-list
+         get-stack-list
+         get-current-symbol
+         get-stack-top)
+
+;; configuration-> string  Return a string representing the tape
 (define (configuration->string config)
   (if (not (list? config))
       "--"
@@ -91,7 +146,13 @@
              [stack-string (list->string (get-stack-list config))])
         (string-append state-string ": " tape-string "; " stack-string))))
 
-(provide configuration->string)
+(provide make-config
+         get-current-state
+         get-tape-list
+         get-stack-list
+         get-current-symbol
+         get-stack-top
+         configuration->string)
 
 
 ;; =============================
@@ -132,121 +193,121 @@
 (provide step)
 
 
-;; ===================================================
-;; Run a computation
-
-;; show-state-config  Print one line with state and current configuration information
-(define (show-step-config s c)
-  (printf "Step ~a: ~a\n" (number->string s)
-          (configuration->string c))
-  )
-
-;; run  Run a FSM computation
-;(define (run fsm sigma)
-;  (define (run-helper config step)
-;    (let ([tape-list (get-tape-list config)]
-;          [current-state (get-current-state config)])
-;      (if (null? tape-list)
-;                 current-state
-;                 (begin
-;                   (show-step-config step config)
-;                   (run-helper (make-config (delta fsm current-state (car tape-list))
-;                                          (cdr tape-list))
-;                               (+ 1 step))
-;                   ))))
-;  ;
-;  (run-helper (make-config 0
-;                           (string->list sigma))
-;              0))
-
-(define (run pdm sigma)
-  (let* ([config (make-config 0
-                              (string->list sigma)
-                              (list BOT))]
-         [step-no 0])
-    (show-step-config step-no config)
-    (for ([current-symbol (get-tape-list config)])
-      (set! step-no (+ 1 step-no))
-      (set! config (step pdm config))
-      (show-step-config step-no config))
-    (get-current-state config)))
-
-
-(define (decide pdm F sigma)
-  (if (member (run pdm sigma) F)
-      "accept"
-      "reject"))
-
-(provide run
-         decide)
-
-;; ======================================================
-;; Read machine from a file
-;; string->instruction  Convert a string, a line from the file, to a single instruction
-(define (current-state-string->number s)
-  (if (eq? #\( (string-ref s 0))   ;; allow instr to start with (
-      (string->number (substring s 1))
-      (string->number s)))
-(define (current-symbol-string->char s)
-  (string-ref s 0))
-(define (next-state-string->number s)
-  (if (eq? #\) (string-ref s (- (string-length s) 1))) ;; ends with )?
-      (string->number (substring s 0 (- (string-length s) 1)))
-      (string->number s)))
-(define (string->instruction s)
-  (let* ([instruction (string-split (string-trim s))]
-         [current-state (current-state-string->number (first instruction))]
-         [current-symbol (current-symbol-string->char (second instruction))]
-         [next-state (next-state-string->number (third instruction))])
-    (list current-state
-          current-symbol
-          next-state)))
-
-(provide string->instruction)
-
-;; ........................
-;; Command line to invoke this program
-
-(define verbose? (make-parameter #f))
-(define fsm-filename (make-parameter null))
-(define inputstring (make-parameter ""))  
-; (define statelimit (make-parameter "1000")) ;; max number of steps simulator runs
-
-(define command-line-parser
-  (command-line
-   #:usage-help 
-   "Simulate a Finite State machine."
-   "Put instructions like `state-number current-char next-state-number' on separate lines."
-   #:once-each
-   [("-v" "--verbose") "Verbose mode" (verbose? #t)]
-   [("-f" "--filename") fsmfn "Name of file with the Finite State machine" (fsm-filename fsmfn)]
-   [("-i" "--input-string") in-st "String giving nonblank tape contents" (inputstring in-st)]
-   ; [("-s" "--statelimit") slmt "Number of steps to run" (statelimit slmt)]
-   #:args  () (void)))
-
-(define FSM-LINES '())  ;; list of file lines, one string per instruction
-;; This is for allowing input from the command line
-(if (null? (fsm-filename))
-    (set! FSM-LINES '())
-    (set! FSM-LINES (file->lines (fsm-filename) #:mode 'text #:line-mode 'any)))
-
-;; for debugging:
-;FSM-LINES
-
-;; Return the list with the last element omitted
-(define (omit-last-element lst)
-  (reverse (cdr (reverse lst))))
-
-;; Note that empty lines give an error in the TM
-;(unless (non-empty-string? (last FSM-LINES))
-;  (fprintf (current-output-port)
-;           "File ~s: Trailing empty string will cause an error.  Delete it.\n"
-;           (fsm-filename)))
-
-;; Return a list of instructions
-(define FSM (for/list ([line FSM-LINES])
-                 (string->instruction line)))
-FSM
-
-
-(run FSM (inputstring))
+;;; ===================================================
+;;; Run a computation
+;
+;;; show-state-config  Print one line with state and current configuration information
+;(define (show-step-config s c)
+;  (printf "Step ~a: ~a\n" (number->string s)
+;          (configuration->string c))
+;  )
+;
+;;; run  Run a FSM computation
+;;(define (run fsm sigma)
+;;  (define (run-helper config step)
+;;    (let ([tape-list (get-tape-list config)]
+;;          [current-state (get-current-state config)])
+;;      (if (null? tape-list)
+;;                 current-state
+;;                 (begin
+;;                   (show-step-config step config)
+;;                   (run-helper (make-config (delta fsm current-state (car tape-list))
+;;                                          (cdr tape-list))
+;;                               (+ 1 step))
+;;                   ))))
+;;  ;
+;;  (run-helper (make-config 0
+;;                           (string->list sigma))
+;;              0))
+;
+;(define (run pdm sigma)
+;  (let* ([config (make-config 0
+;                              (string->list sigma)
+;                              (list BOT))]
+;         [step-no 0])
+;    (show-step-config step-no config)
+;    (for ([current-symbol (get-tape-list config)])
+;      (set! step-no (+ 1 step-no))
+;      (set! config (step pdm config))
+;      (show-step-config step-no config))
+;    (get-current-state config)))
+;
+;
+;(define (decide pdm F sigma)
+;  (if (member (run pdm sigma) F)
+;      "accept"
+;      "reject"))
+;
+;(provide run
+;         decide)
+;
+;;; ======================================================
+;;; Read machine from a file
+;;; string->instruction  Convert a string, a line from the file, to a single instruction
+;(define (current-state-string->number s)
+;  (if (eq? #\( (string-ref s 0))   ;; allow instr to start with (
+;      (string->number (substring s 1))
+;      (string->number s)))
+;(define (current-symbol-string->char s)
+;  (string-ref s 0))
+;(define (next-state-string->number s)
+;  (if (eq? #\) (string-ref s (- (string-length s) 1))) ;; ends with )?
+;      (string->number (substring s 0 (- (string-length s) 1)))
+;      (string->number s)))
+;(define (string->instruction s)
+;  (let* ([instruction (string-split (string-trim s))]
+;         [current-state (current-state-string->number (first instruction))]
+;         [current-symbol (current-symbol-string->char (second instruction))]
+;         [next-state (next-state-string->number (third instruction))])
+;    (list current-state
+;          current-symbol
+;          next-state)))
+;
+;(provide string->instruction)
+;
+;;; ........................
+;;; Command line to invoke this program
+;
+;(define verbose? (make-parameter #f))
+;(define fsm-filename (make-parameter null))
+;(define inputstring (make-parameter ""))  
+;; (define statelimit (make-parameter "1000")) ;; max number of steps simulator runs
+;
+;(define command-line-parser
+;  (command-line
+;   #:usage-help 
+;   "Simulate a Finite State machine."
+;   "Put instructions like `state-number current-char next-state-number' on separate lines."
+;   #:once-each
+;   [("-v" "--verbose") "Verbose mode" (verbose? #t)]
+;   [("-f" "--filename") fsmfn "Name of file with the Finite State machine" (fsm-filename fsmfn)]
+;   [("-i" "--input-string") in-st "String giving nonblank tape contents" (inputstring in-st)]
+;   ; [("-s" "--statelimit") slmt "Number of steps to run" (statelimit slmt)]
+;   #:args  () (void)))
+;
+;(define FSM-LINES '())  ;; list of file lines, one string per instruction
+;;; This is for allowing input from the command line
+;(if (null? (fsm-filename))
+;    (set! FSM-LINES '())
+;    (set! FSM-LINES (file->lines (fsm-filename) #:mode 'text #:line-mode 'any)))
+;
+;;; for debugging:
+;;FSM-LINES
+;
+;;; Return the list with the last element omitted
+;(define (omit-last-element lst)
+;  (reverse (cdr (reverse lst))))
+;
+;;; Note that empty lines give an error in the TM
+;;(unless (non-empty-string? (last FSM-LINES))
+;;  (fprintf (current-output-port)
+;;           "File ~s: Trailing empty string will cause an error.  Delete it.\n"
+;;           (fsm-filename)))
+;
+;;; Return a list of instructions
+;(define FSM (for/list ([line FSM-LINES])
+;                 (string->instruction line)))
+;FSM
+;
+;
+;(run FSM (inputstring))

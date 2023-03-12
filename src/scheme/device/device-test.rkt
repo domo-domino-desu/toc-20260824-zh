b7dc747c81b79a980473f913ddd6b0eef9aa2f0a
Jim Hefferon <jhefferon@smcvt.edu>
2023-03-12 14:23:30 -0400

    refactor to remove global vars in device

--- src/scheme/device/device-test.rkt
+++ src/scheme/device/device-test.rkt
@@ -9,40 +9,37 @@
 ;; License: GPL 3.0
 
 
-;; ===== DELTA tests
-(define DELTA-tests
+;; ===== delta tests
+(define delta-tests
   (test-suite
-   "initialize and manipulate DELTA"
+   "initialize and manipulate delta-map and delta"
   
    (test-case
-    "Test making DELTA"
-    ; (printf "DELTA=~s\n" DELTA)
-    (let ([input (list 0 "a")]
+    "Test making delta-map"
+    (let ([delta-map (make-delta-map)]
+          [input (list 0 "a")]
           [other-input (list 3 "b")]
           [output (list 1)]
           [other-output (list 2)])
-      (make-DELTA)
-      (DELTA-set! input output)
-      ; (printf "DELTA=~s\n" DELTA)
-      (DELTA-set! input other-output)
-      ; (printf "DELTA=~s\n" DELTA)
-      (DELTA-set! other-input output)
-      ; (printf "DELTA=~s\n" DELTA)
-      (check-true (set-member? (delta input) output))
-      (check-true (set-member? (delta input) other-output))
-      (check-true (set-member? (delta other-input) output))
+      (set-delta-map! delta-map input output)
+      (check-true (set-member? (delta delta-map input) output))
+      (set-delta-map! delta-map input other-output)
+      (set-delta-map! delta-map other-input output)
+      (check-true (set-member? (delta delta-map input) output))
+      (check-true (set-member? (delta delta-map input) other-output))
+      (check-true (set-member? (delta delta-map other-input) output))
       )
     );; end test-case
 
    (test-case
     "Test delta function"
-    (let ([input (list 0 "a")]
+    (let ([delta-map (make-delta-map)]
+          [input (list 0 "a")]
           [other-input (list 3 "b")]
           [output (list 1)])
-      (reset-DELTA)
-      (DELTA-set! input output)
-      (check-true (set-member? (delta input) output))
-      (check-equal? (delta other-input) DELTA-NOKEY)
+      (set-delta-map! delta-map input output)
+      (check-true (set-member? (delta delta-map input) output))
+      (check-equal? (delta delta-map other-input) DELTA-NOKEY)
       )
     );; end test-case
 
@@ -50,89 +47,138 @@
 
 
 ;; ===== tapestruct tests
-(define TAPE-tests
+(define tape-tests
   (test-suite
-   "initialize and manipulate TAPE"
+   "initialize and manipulate tape"
   
-;   (test-case
-;    "Test making TAPE"
-;    ; (make-TAPE)
-;    ; (printf "TAPE=~s\n" TAPE)
-;    (make-tape)
-;    (check-equal? (get-tape-right) '())
-;    (check-equal? (get-tape-current) " ")
-;    (make-tape "a" "b" "B")
-;    (check-equal? (get-tape-right) (list "b" "B"))
-;    (check-equal? (get-tape-current) "a")
-;    ; (printf "TAPE=~s\n" TAPE)
-;    );; end test-case
+   (test-case
+    "Test making tape"
+    (let ([tape (make-tape)])
+      (check-equal? (get-tape-right tape) '())
+      (check-equal? (get-tape-left tape) '())
+      (check-true (or (equal? (get-tape-current tape) " ")
+                      (equal? (get-tape-current tape) BLANK)))
+      )
+    (let ([tape (make-tape "a" "b" "B")])
+      (check-equal? (get-tape-right tape) (list "b" "B"))
+      (check-equal? (get-tape-left tape) '())
+      (check-equal? (get-tape-current tape) "a")
+      )
+    (let ([tape (make-tape "a")])
+      (check-equal? (get-tape-right tape) '())
+      (check-equal? (get-tape-left tape) '())
+      (check-equal? (get-tape-current tape) "a")
+      )
+    );; end test-case
   
-;   (test-case
-;    "Test trimming tape"
-;    (let ([left-tape '(" " " " "b")]
-;          [right-tape '("a" " ")])
-;      (check-equal? (trim-left-tape left-tape) '("b"))
-;      (check-equal? (trim-right-tape right-tape) '("a"))
-;      )
-;    (let ([left-tape '("B" " " "b")]
-;          [right-tape '("a" "B")])
-;      (check-equal? (trim-left-tape left-tape) '("b"))
-;      (check-equal? (trim-right-tape right-tape) '("a"))
-;      )
-;    (let ([left-tape '(" " " ")]
-;          [right-tape '(" " "B")])
-;      (check-equal? (trim-left-tape left-tape) '())
-;      (check-equal? (trim-right-tape right-tape) '())
-;      )
-;    (let ([left-tape '("a" " " "b" " ")]
-;          [right-tape '(" " "a" " " "b")])
-;      (check-equal? (trim-left-tape left-tape) left-tape)
-;      (check-equal? (trim-right-tape right-tape) right-tape)
-;      )
-;    (make-TAPE)
-;    (set-TAPE! '(" " "B" "z") " " '(" "))
-;    (trim-tape)
-;    (check-equal? (get-tape-left) '("z"))
-;    (check-equal? (get-tape-right) '())
-;    (check-equal? (get-tape-current) " ")
-;    );; end test-case
+   (test-case
+    "Test trimming tape"
+    (let ([left-tape '(" " " " "b")]
+          [right-tape '("a" " ")])
+      (check-equal? (trim-left-tape left-tape) '("b"))
+      (check-equal? (trim-right-tape right-tape) '("a"))
+      )
+    (let ([left-tape '("B" " " "b")]
+          [right-tape '("a" "B")])
+      (check-equal? (trim-left-tape left-tape) '("b"))
+      (check-equal? (trim-right-tape right-tape) '("a"))
+      )
+    (let ([left-tape '(" " " ")]
+          [right-tape '(" " "B")])
+      (check-equal? (trim-left-tape left-tape) '())
+      (check-equal? (trim-right-tape right-tape) '())
+      )
+    (let ([left-tape '("a" " " "b" " ")]
+          [right-tape '(" " "a" " " "b")])
+      (check-equal? (trim-left-tape left-tape) left-tape)
+      (check-equal? (trim-right-tape right-tape) right-tape)
+      )
+    (let ([tape (make-tape)])
+      (set-tape! tape '(" " "B" "z") "B" '(" "))
+      (trim-tape tape)
+      (check-equal? (get-tape-left tape) '("z"))
+      (check-equal? (get-tape-right tape) '())
+      (check-true (or (equal? (get-tape-current tape) " ")
+                      (equal? (get-tape-current tape) BLANK)))
+      )
+    );; end test-case
 
    (test-case
     "Test moving head against TAPE"
-    (make-tape "a" "b" "B")
-    (move-head-right)
-    (check-equal? (get-tape-left) (list "a"))
-    (check-equal? (get-tape-right) (list "B"))
-    (check-equal? (get-tape-current) "b")
-    (move-head-left)
-    (check-equal? (get-tape-left) '())
-    (check-equal? (get-tape-right) (list "b" "B"))
-    (check-equal? (get-tape-current) "a")
-    (make-tape)
-    (check-equal? (get-tape-right) '())
-    (check-equal? (get-tape-left) '())
-    (check-equal? (get-tape-current) " ")
-    (move-head-right)
-    (trim-tape)
-    (check-equal? (get-tape-left) '())
-    (check-equal? (get-tape-right) '())
-    (check-equal? (get-tape-current) " ")
-    (move-head-left)
-    (trim-tape)
-    (check-equal? (get-tape-left) '())
-    (check-equal? (get-tape-right) '())
-    (check-equal? (get-tape-current) " ")
-    
-    ; (printf "TAPE=~s\n" TAPE)
+    (let ([tape (make-tape "a" "b" "B")])
+      (move-head-right tape)
+      (check-equal? (get-tape-left tape) (list "a"))
+      (check-equal? (get-tape-right tape) (list "B"))
+      (check-equal? (get-tape-current tape) "b")
+      (move-head-left tape)
+      (check-equal? (get-tape-left tape) '())
+      (check-equal? (get-tape-right tape) (list "b" "B"))
+      (check-equal? (get-tape-current tape) "a"))
+    (let ([tape (make-tape)])
+      (check-equal? (get-tape-right tape) '())
+      (check-equal? (get-tape-left tape) '())
+      (check-true (or (equal? (get-tape-current tape) " ")
+                      (equal? (get-tape-current tape) BLANK)))
+      (move-head-right tape)
+      (trim-tape tape)
+      (check-equal? (get-tape-left tape) '())
+      (check-equal? (get-tape-right tape) '())
+      (check-true (or (equal? (get-tape-current tape) " ")
+                      (equal? (get-tape-current tape) BLANK)))
+      (move-head-left tape)
+      (trim-tape tape)
+      (check-equal? (get-tape-left tape) '())
+      (check-equal? (get-tape-right tape) '())
+      (check-true (or (equal? (get-tape-current tape) " ")
+                      (equal? (get-tape-current tape) BLANK)))
+      )
     );; end test-case
 
-
-   
    )) ;; end TAPE-tests suite and tests
 
+
+;; ===== Stack making
+(define stack-making-tests
+  (test-suite
+   "stack making tests"
+
+   (test-case
+    "Test making the stack"
+    (let ([stack (make-stack "g0" "g1")])
+      (check = 3 (length stack))
+      (check equal? "g0" (first stack))
+      (check equal? "g1" (second stack))
+      (check equal? BOT (third stack))))
+   (test-case
+    "Test making the empty stack"
+    (let ([stack (make-stack)])
+      (check = 1 (length stack))
+      (check equal? BOT (first stack))))
+
+   ; Stack operations
+   (test-case
+    "Test stack push and pop operations"
+    (let ([st (make-stack "g0" "g1" "g2")])
+      (check = (+ 1 (length st)) (length (stack-push #\w st)))
+      (check = (- (length st) 1) (length (stack-pop st)))
+      (check equal? (cdr st) (stack-pop st))
+      (check equal? "g0" (stack-top st))
+      (check equal? st (stack-push "g0" (stack-pop st)))
+      (check equal? st (stack-pop (stack-push "g3" st)))
+      (check equal? (make-stack "g3" "g2" "g0" "g1" "g2") (stack-push-list (list "g3" "g2") st))
+      ))
+   (test-case
+    "Test stack-bot? operation"
+    (let ([st0 (make-stack "g0" "g1" "g2")]
+          [st1 (make-stack)])
+      (check-false (stack-bot? st0))
+      (check-true (stack-bot? st1))
+      ))
+   )) ;; end stack-making suite and tests
 
 
 
 ;; ===== Run the tests; comment out ones not being worked-on
-; (run-tests DELTA-tests)
-(run-tests TAPE-tests)
+; (run-tests delta-tests)
+(run-tests tape-tests)
+; (run-tests stack-making-tests)
--- src/scheme/device/device.rkt
+++ src/scheme/device/device.rkt
@@ -18,88 +18,67 @@
 ;; ===== DELTA
 ;; Finite function mapping lists to sets.  The lists are input tuples, the
 ;; sets contain output tuples 
-(define DELTA '())
 
-;; Make DELTA be a new hash
-(define (make-DELTA)
-  (set! DELTA (make-hash)))
-;; Clear DELTA to the empty list, not a hash
-(define (clear-DELTA)
-  (set! DELTA '()))
-;; Reset DELTA to an empty hash 
-(define (reset-DELTA)
-  (make-DELTA))
+;; void -> hash
+;; Make a new delta be a new hash
+(define (make-delta-map)
+  (make-hash))
 
 ;; list, list -> void
 ;; Add the value to the set that is DELTA[k], or create a new set if none there
-(define (DELTA-set! k v)
-  (if (member k (hash-keys DELTA))
-      (set-add! (hash-ref DELTA k) v)
-      (hash-set! DELTA k (mutable-set v))))
+(define (set-delta-map! delta-map k v)
+  (if (member k (hash-keys delta-map))
+      (set-add! (hash-ref delta-map k) v)
+      (hash-set! delta-map k (mutable-set v))))
 
 ;; Signifies DELTA has no such key
-(define DELTA-NOKEY "No such key for Delta")
+(define DELTA-NOKEY "No such key for delta map")
 
 ;; list -> list
 ;; Return value associated with k in DELTA, or DELTA-NOKEY
-(define (delta k)
-  (hash-ref DELTA k (lambda () DELTA-NOKEY)))
+(define (delta delta-map k)
+  (hash-ref delta-map k (lambda () DELTA-NOKEY)))
 
-(provide DELTA
-         make-DELTA
-         clear-DELTA
-         reset-DELTA
-         DELTA-set!
+(provide make-delta-map
+         set-delta-map!
          DELTA-NOKEY
          delta)
 
 
 ;; ===== tape
+
 ;; A tape is a struct.
 (struct tapestruct (left current right) #:transparent #:mutable)
 
 ;; Sometimes it is convenient to have a B instead of just a blank space
 (define BLANK "B")
 
-;; We use TAPE as the global tape.  Here it has the initial value '() as a way
-;; to tell that it is undefined
-(define TAPE '())
-
-;; -> void
-;; Create a new tapestruct
-(define (make-TAPE)
-  (set! TAPE (tapestruct '() " " '())))
-
-;; list of strings, string, list of strings -> void
-;; Set the TAPE structure to have the given left, current, and right
-(define (set-TAPE! left current right)
-  (set-tapestruct-left! TAPE left)
-  (set-tapestruct-current! TAPE current)
-  (set-tapestruct-right! TAPE right))
-
-;; Set TAPE to be a dummy value
-(define (clear-TAPE)
-  (set! TAPE '()))
-
-; list of strings  ->  tape-list
-; Make a tape list, with I/O head pointing to first token
+; list of strings  ->  tape structure
+; Make a tape structure. The I/O head points to the first token (or BLANK)
 (define (make-tape . tape-tokens)
   ; (printf "make-tape: tape-tokens are ~s\n" tape-tokens)
-  (make-TAPE)
-  (if (null? tape-tokens)
-      (set-tapestruct-current! TAPE " ")
-      (begin
-        (set-tapestruct-current! TAPE (car tape-tokens))
-        (set-tapestruct-right! TAPE (cdr tape-tokens)))))
+  (let ([tape (tapestruct '() BLANK '())])
+    (when (not (null? tape-tokens))
+      (set-tapestruct-current! tape (car tape-tokens))
+      (set-tapestruct-right! tape (cdr tape-tokens)))
+    tape))
 
-;; -> void
+;; tape structure, list of strings, string, list of strings -> tape structure
+;; Set the tape to have the given left, current, and right
+(define (set-tape! tape left current right)
+  (set-tapestruct-left! tape left)
+  (set-tapestruct-current! tape current)
+  (set-tapestruct-right! tape right)
+  tape)
+
+;; void -> void
 ;; Get the left tape, the current token, or the right tape 
-(define (get-tape-left)
-  (tapestruct-left TAPE))
-(define (get-tape-current)
-  (tapestruct-current TAPE))
-(define (get-tape-right)
-  (tapestruct-right TAPE))
+(define (get-tape-left tape)
+  (tapestruct-left tape))
+(define (get-tape-current tape)
+  (tapestruct-current tape))
+(define (get-tape-right tape)
+  (tapestruct-right tape))
 
 ;; list of strings -> list of strings
 ;; Omit leftmost " " or "B" tokens
@@ -117,59 +96,49 @@
 (define (trim-right-tape t)
   (reverse (trim-left-tape (reverse t))))
 
-;; -> void
-;; From the TAPE, trim " " or "B" off the left tape or right tape 
-(define (trim-tape)
-  (let ([tape-left (get-tape-left)]
-        [tape-current (get-tape-current)]
-        [tape-right (get-tape-right)])
-    (set-TAPE! (trim-left-tape tape-left) tape-current (trim-right-tape tape-right))))
+;; tape -> tape
+;; Trim any extra " " or "B" off the left tape or right tape 
+(define (trim-tape tape)
+  (let ([tape-left (get-tape-left tape)]
+        [tape-current (get-tape-current tape)]
+        [tape-right (get-tape-right tape)])
+    (set-tape! tape (trim-left-tape tape-left) tape-current (trim-right-tape tape-right))))
 
-;; -> string
-;; Move the head right on the TAPE structure.  Same as moving the tape left.  Return the new
-;; current token.
-(define (move-head-right)
-  (let ([tape-left (get-tape-left)]
-        [tape-current (get-tape-current)]
-        [tape-right (get-tape-right)]
-        [new-tape-left '()]
-        [new-tape-current " "]
-        [new-tape-right '()])
-    (set! new-tape-left (reverse (cons tape-current (reverse tape-left))))
-    (if (null? tape-right)
-        (set! new-tape-current " ")
-        (begin
-          (set! new-tape-current (car tape-right))
-          (set! new-tape-right (cdr tape-right))))
-    (set! TAPE (tapestruct new-tape-left new-tape-current new-tape-right))
-    new-tape-current
+;; tape -> string
+;; Move the head right on the tape structure.  Same as moving the tape left.  Return the changed tape.
+(define (move-head-right tape)
+  (let* ([tape-left (get-tape-left tape)]
+         [tape-current (get-tape-current tape)]
+         [tape-right (get-tape-right tape)]
+         [new-tape-left (reverse (cons tape-current (reverse tape-left)))]
+         [new-tape-current BLANK]
+         [new-tape-right '()])
+    (when (not (null? tape-right))
+      (set! new-tape-current (car tape-right))
+      (set! new-tape-right (cdr tape-right)))
+    (set-tape! tape new-tape-left new-tape-current new-tape-right)
     ))
 
-;; -> string
-;; Move the head left on the TAPE structure.  Same as moving the tape right.  Return the new
-;; current token.
-(define (move-head-left)
-  (let ([tape-left (get-tape-left)]
-        [tape-current (get-tape-current)]
-        [tape-right (get-tape-right)]
-        [new-tape-left '()]
-        [new-tape-current " "]
-        [new-tape-right '()])
-    (set! new-tape-right (cons tape-current tape-right))
-    (if (null? tape-left)
-        (set! new-tape-current " ")
+;; tape -> string
+;; Move the head left on the tape structure.  Same as moving the tape right.  Return the changed tape.
+(define (move-head-left tape)
+  (let* ([tape-left (get-tape-left tape)]
+         [tape-current (get-tape-current tape)]
+         [tape-right (get-tape-right tape)]
+         [new-tape-left '()]
+         [new-tape-current BLANK]
+         [new-tape-right (cons tape-current tape-right)])
+    (when (not (null? tape-left))
         (let ([reversed-tape-left (reverse tape-left)])
           (set! new-tape-current (car reversed-tape-left))
           (set! new-tape-left (reverse (cdr reversed-tape-left)))))
-    (set! TAPE (tapestruct new-tape-left new-tape-current new-tape-right))
-    new-tape-current
+    (set-tape! tape new-tape-left new-tape-current new-tape-right)
     ))
 
 (provide tapestruct
-         TAPE
-         make-TAPE
-         set-TAPE!
+         BLANK
          make-tape
+         set-tape!
          get-tape-left
          get-tape-current
          get-tape-right
@@ -178,3 +147,54 @@
          trim-tape
          move-head-left
          move-head-right)
+
+
+;; ===== stack
+
+;; A stack is a list of strings, where the car is the top of the stack
+
+(define BOT "BOT")  ;; Stack bottom
+(define STACK-EXHAUSTED "stack exhausted")  ;; Attempt to read an empty stack
+
+; list of strings -> stack-list
+; Make a stack, ending with a bottom marker
+(define (make-stack . stack-tokens)
+  (append stack-tokens (list BOT)))
+
+; stack-list ->  stack-list or STACK-EXHAUSTED
+; Pop the top token off the stack.  Return STACK-EXHAUSTED if stack is empty.
+(define (stack-pop stack-list)
+  (if (null? stack-list)
+      STACK-EXHAUSTED
+      (cdr stack-list)))
+
+; stack-list ->  string or STACK-EXHAUSTED
+; Name top token on the stack, without popping.  Return STACK-EXHAUSTED if stack is empty.
+(define (stack-top stack-list)
+  (if (null? stack-list)
+      STACK-EXHAUSTED
+      (first stack-list)))
+
+; string stack-list ->  stack-list
+; Push a new top token onto the stack.
+(define (stack-push ch stack-list)
+  (cons ch stack-list))
+
+; string-list stack-list ->  stack-list
+; Push the list of tokens onto the stack.
+(define (stack-push-list token-list stack-list)
+  (append token-list stack-list))
+
+; stack-list -> bool
+; Decide if the stack's top token is BOT, that is, whether the stack is exhausted.
+(define (stack-bot? stack-list)
+  (equal? BOT (car stack-list)))
+
+(provide BOT
+         STACK-EXHAUSTED
+         make-stack
+         stack-pop
+         stack-top
+         stack-push
+         stack-push-list
+         stack-bot?)

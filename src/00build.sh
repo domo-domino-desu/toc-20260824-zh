942c2b78ef0cbc5d5df516c8efc0f4d387b4f523
Jim Hefferon <jhefferon@smcvt.edu>
2023-06-27 15:50:34 -0400

    Try to adjust to meet upload reqs

--- src/00build.sh
+++ src/00build.sh
@@ -1,16 +1,7 @@
 #!/bin/bash
 # LaTeX the Computation book and answers
 
-# tex4ht messes with this stuff
-# rm prologue/prologue.aux
-# rm background/background.aux
-# rm languages/languages.aux
-# rm automata/automata.aux
-# rm complexity/complexity.aux
-# rm appendix/appendix.aux
-
-
-# Generate the book; run it twice to settle future references
+# Generate the book; run it twice to settle any future references
 pdflatex book
 makeindex -s book.ist -p odd book.idx
 biber book

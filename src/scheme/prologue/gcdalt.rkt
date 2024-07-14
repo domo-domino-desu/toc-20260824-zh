318338ec7d8599d805a8c098390f42186fcc18b9
Jim Hefferon <jim.hefferon@gmail.com>
2024-07-14 11:43:15 -0400

    combine the two gcd questions in prim rec section

--- src/scheme/prologue/gcdalt.rkt
+++ src/scheme/prologue/gcdalt.rkt
@@ -1,18 +1,35 @@
 ;; GCD  Compute the GCD of two numbers
-;; Use a recurrence based on these rules:
-;; gcd(x+y,y)=gcd(x,y)
-;; gcd(x,y)=gcd(y,x)
-;; gcd(x,x)=x
-;; gcd(x,1)=1
-
+;; Use a recurrence based on Euclid's observation that if a>b then the set
+;; of common divisors of a and b is the same as the set of common divisors
+;; of a-b and b
 #lang racket
-(define (gcd a b)
+
+(define (gcd-first a b)
   (cond
-    [(= a b)
-     a]
-    [(= b 1)
-     1]
-    [(> b a)
-     (gcd b a)]
-    [else
-     (gcd (- a b) b)]))
+    [(zero? a) b]
+    [(zero? b) a]
+    [(> b a) (gcd-first b a)]
+    [else (gcd-first (- a b) b)]))
+
+(module+ test
+  (require rackunit)
+  (check-equal? (gcd-first 9 12) 3)
+  (check-equal? (gcd-first 256 125) 1)
+  (check-equal? (gcd-first 0 7) 7)
+  (check-equal? (gcd-first 5 0) 5)
+  (check-equal? (gcd-first 0 0) 0)
+  )
+
+(define (gcd-second a b)
+  (if (zero? b)
+      a
+      (gcd-second b (remainder a b))))
+
+(module+ test
+  (require rackunit)
+  (check-equal? (gcd-second 9 12) 3)
+  (check-equal? (gcd-second 256 125) 1)
+  (check-equal? (gcd-second 0 7) 7)
+  (check-equal? (gcd-second 5 0) 5)
+  (check-equal? (gcd-second 0 0) 0)
+  )

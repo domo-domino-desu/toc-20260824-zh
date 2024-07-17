97e22c332714e8f373bd1751d533299dc0d4a2a3
Jim Hefferon <jim.hefferon@gmail.com>
2024-07-17 10:42:32 -0400

    wording on recursion exercises

--- src/scheme/prologue/mu-addition.rkt
+++ src/scheme/prologue/mu-addition.rkt
@@ -7,9 +7,24 @@
       1))
 
 (define (f x)
-  (define (f-helper x y)
+  (define (f-helper y)  ; value of x inherited from enclosing defn of f
     (if (= 0 (g x y))
         y
-        (f-helper x (+ 1 y))))
-  (let ([y 0])
-    (f-helper x 0)))
+        (f-helper (+ 1 y))))
+  
+  (f-helper 0))
+
+
+;; Like the prior one but using multiplication
+(define (g-mult x y)
+  (if (= 100 (* x y))
+      0
+      1))
+
+(define (f-mult x)
+  (define (f-helper y)  ; value of x inherited from enclosing defn of f-mult
+    (if (= 0 (g-mult x y))
+        y
+        (f-helper (+ 1 y))))
+  
+  (f-helper 0))

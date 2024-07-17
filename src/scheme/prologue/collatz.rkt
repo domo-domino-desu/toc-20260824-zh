2081452c958c465ed9d4845dd0fd765d54dff924
Jim Hefferon <jim.hefferon@gmail.com>
2024-07-17 17:52:32 -0400

    verify all exercises in general recursion

--- src/scheme/prologue/collatz.rkt
+++ src/scheme/prologue/collatz.rkt
@@ -15,3 +15,12 @@
         (C-helper (H n) (add1 k))))
 
   (C-helper n 0))
+
+(define (C-verbose n)
+  (define (C-verbose-helper n k)
+      (printf "n=~a k=~a H(n)=~a\n" n k (H n))
+      (if (= 1 n)
+          k
+          (C-verbose-helper (H n) (add1 k))))
+
+  (C-verbose-helper n 0))
--- src/scheme/prologue/fxequalsx-exercise.rkt
+++ src/scheme/prologue/fxequalsx-exercise.rkt
@@ -1,20 +1,16 @@
 #lang racket
 ;; This pertains to an exercise in the subsection on general recursion
-
-;; g Function given in exercise
 (define (g x y)
   (ceiling (- (/ (add1 x) (add1 y))
               1)))
 
-;; f  Compute from g by mu-recursion
 (define (f x)
   (define (f-helper y)
     (if (= 0 (g x y))
         y
         (f-helper (add1 y))))
 
-  (let ([y 0])
-    (f-helper y)));; f  Compute from g by mu-recursion
+    (f-helper 0))
 
 (define (f-verbose x)
   (define (f-verbose-helper y)
@@ -23,5 +19,4 @@
         y
         (f-verbose-helper (add1 y))))
 
-  (let ([y 0])
-    (f-verbose-helper y)))
+    (f-verbose-helper 0))

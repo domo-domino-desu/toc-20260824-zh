678f7f893607037e9e3b0bb78b796f99fe055d29
Jim Hefferon <jim.hefferon@gmail.com>
2025-04-07 18:20:06 -0400

    prologue reduce clauses

--- src/00build.sh
+++ src/00build.sh
@@ -4,10 +4,10 @@ LATEX=pdflatex
 
 # Generate the book; run it twice to settle any future references
 ${LATEX} book
-makeindex -s book.ist -p odd book.idx
-biber book
-${LATEX} book
-${LATEX} answers
-biber answers
-${LATEX} answers
+# makeindex -s book.ist -p odd book.idx
+# biber book
+# ${LATEX} book
+# ${LATEX} answers
+# biber answers
+# ${LATEX} answers
 

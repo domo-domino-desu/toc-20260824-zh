1398d213074372364ad808d9430f8e082ca8e150
Jim Hefferon <jim.hefferon@gmail.com>
2025-04-12 06:43:49 -0400

    wording I.3, and exercises

--- src/scheme/prologue/primitive-recursive.rkt
+++ src/scheme/prologue/primitive-recursive.rkt
@@ -11,7 +11,7 @@
 
 ;; plus  Addition of two natural numbers
 (define (plus x y)
-  (let ((z (- y 1)))
+  (let ([z (- y 1)])
     (if (= y 0)
         x
         (successor (plus x z)))))
@@ -20,7 +20,7 @@
 
 ;; product  Multiplication of two natural numbers
 (define (product x y)
-  (let ((z (- y 1)))
+  (let ([z (- y 1)])
     (if (= y 0)
         0
         (plus (product x z) x))))
@@ -29,7 +29,7 @@
 
 ;; power  Exponentiation of two natural numbers
 (define (power x y)
-  (let ((z (- y 1)))
+  (let ([z (- y 1)])
     (if (= y 0)
         1
         (product (power x z) x))))

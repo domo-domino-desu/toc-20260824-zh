afc8251ba6c665a31fd5810c5e7699f9296108aa
Jim Hefferon <jim.hefferon@gmail.com>
2025-05-21 15:30:39 -0400

    converting cleveref to zref-clever, got book.pdf to mostly work

--- src/00build.sh
+++ src/00build.sh
@@ -6,11 +6,11 @@ LATEX=pdflatex
 ${LATEX} book
 makeindex -s book.ist -p odd book.idx
 # Can't figure out how to have both creferences to numberedwithin "Lemma" and Index numbers, so doing the first, and have a script to compensate for the second
-../bin/compilation/repage_index.py book.ind > book_ind.repaged 
-cp book_ind.repaged book.ind
-biber book
+# ../bin/compilation/repage_index.py book.ind > book_ind.repaged 
+# cp book_ind.repaged book.ind
+# biber book
 ${LATEX} book
-${LATEX} answers
-biber answers
-${LATEX} answers
+# ${LATEX} answers
+# biber answers
+# ${LATEX} answers
 

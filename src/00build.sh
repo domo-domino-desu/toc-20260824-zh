44901df5d16ad7a390ba40e4c87f6684642c299c
Jim Hefferon <jim.hefferon@gmail.com>
2025-06-08 12:53:36 -0400

    test slide making also

--- src/00build.sh
+++ src/00build.sh
@@ -5,8 +5,8 @@ LATEX=pdflatex
 # Generate the book; run it twice to settle any future references
 ${LATEX} book
 makeindex -s book.ist -p odd book.idx
-# biber book
-# ${LATEX} book
+biber book
+${LATEX} book
 ${LATEX} answers
-# biber answers
-# ${LATEX} answers
+biber answers
+${LATEX} answers

c775c1ff97e7cf02c8c78ec99edebb41a858f89a
Jim Hefferon <jim.hefferon@gmail.com>
2025-07-29 14:42:35 -0400

    index wording thru d's

--- bin/lines_of_code.sh
+++ bin/lines_of_code.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 # Count lines of code in various files
 cd ../src
-find . -iname "*.tex" -or -iname "*.asy" -or -iname "*.sty" -iname "*.cls" -iname "*.rkt" -iname "*.sh" | xargs wc -l
+find . -iname "*.tex" -or -iname "*.asy" -or -iname "*.sty" -iname "*.cls" -iname "*.rkt" -iname "*.sh" -iname "Makefile" | xargs wc -l

4869711218e3fe8c651cc9add3c51440cb13e059
Jim Hefferon <jim.hefferon@gmail.com>
2025-08-10 11:09:36 -0400

    edition 2.0

--- src/00build.sh
+++ src/00build.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
-# LaTeX the Computation book and answers
+# LaTeX the Theory of Computation book and answers
 LATEX=pdflatex
 
 # Generate the book; run it twice to settle any future references
 ${LATEX} book
+# Add the traditional joke index entry that points to itself
+# Get the page by compiling, then finding how many pages are 
+# frontmatter, numbered with roman type, and subtract.
+echo "\indexentry{recursion|hyperpage}{434}" >> book.idx
+# Make the index
 makeindex -s book.ist -p odd book.idx
+# Make the bibliography 
 biber book
 ${LATEX} book
-# ${LATEX} answers
-# biber answers
-# ${LATEX} answers
+# Compile the answers
+${LATEX} answers
+biber answers
+${LATEX} answers
